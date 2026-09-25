2d4e95d3f83465e4a0ec6ae849bc397bfa9a7c43
nunezjjn <juan.j.nuniez@gmail.com>
2026-09-25 13:07:16 +0200

    Add flight and agent data structures

---
+++ catalogo.py
@@ -0,0 +1,30 @@
+VUELOS = {
+    "AEA4014": "PMI",
+    "AEA4008": "PMI",
+    "AEA4060": "MAD",
+    "AEA4064": "MAD",
+    "AF1417": "CDG",
+    "BT698": "RIX",
+    "HV6336": "AMS",
+    "HV6338": "AMS",
+    "HV6340": "AMS",
+    "W43186": "OTP",
+    "W64410": "SOF",
+    "W62080": "KRK",
+    "QS1055": "PRG",
+    "W46318": "MXP",
+    "W62386": "BUD",
+    "W95368": "LTN",
+    "W95738": "LGW"
+}
+
+AGENTES = [
+    "JavierP",
+    "Carmela",
+    "MariaC",
+    "Daniel",
+    "Pamela",
+    "Nezahat",
+    "CamilaA",
+    "Delfina"
+]
